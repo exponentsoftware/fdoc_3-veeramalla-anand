20874c64e77d1580410182bd6efac386ff1f9535
veeramalla-anand <anandveeramalla202@gmail.com>
2021-05-20 22:17:20 +0530

    assignment

---
+++ signInAndUp.js
@@ -0,0 +1,63 @@
+const users = [
+    {
+            _id: 'ab12ex',
+            username: 'Alex',
+            email: 'alex@alex.com',
+            password: '123123',
+            createdAt:'17/05/2019 9:00 AM',
+            isLoggedIn: false
+    },
+    {
+            _id: 'fg12cy',
+            username: 'Asab',
+            email: 'asab@asab.com',
+            password: '123456',
+            createdAt:'17/05/2019 9:30 AM',
+            isLoggedIn: true
+    },
+    {
+            _id: 'zwf8md',
+            username: 'Brook',
+            email: 'brook@brook.com',
+            password: '123111',
+            createdAt:'17/05/2019 9:45 AM',
+            isLoggedIn: true
+    },
+    {
+            _id: 'eefamr',
+            username: 'Martha',
+            email: 'martha@martha.com',
+            password: '123222',
+            createdAt:'17/05/2019 9:50 AM',
+            isLoggedIn: false
+    },
+    {
+            _id: 'ghderc',
+            username: 'Thomas',
+            email: 'thomas@thomas.com',
+            password: '123333',
+            createdAt:'17/05/2019 10:00 AM',
+            isLoggedIn: false
+    }
+    ];
+
+    
+    const newUser={
+        email: 'newEmail.com',
+        password: '125462'
+    } 
+
+    function signUp(newuser){
+        if(db.users.find({users: {$elemMatch: {email:newuser.email}}})==null){
+            console.log("user does'nt exists.....create an Account")
+        }
+        else{
+            console.log("account laready exists");
+        }
+    }
+
+    function signIn(newuser){
+        if(db.users.find({users: {$elemMatch: {email:newuser.email}}})){
+            console.log("signing In........")
+        }
+    }
